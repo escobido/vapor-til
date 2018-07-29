a4886fe340a9378b48e04596d83fedb1ecc1f125
escobido <matthew.escobido@gmail.com>
2018-07-29 09:23:27 +0800

    Update Use PostgreSQL as the database

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,19 @@
 import App
+import Service
+import Vapor
 
-try app(.detect()).run()
+var config = Config.default()
+var env = try Environment.detect()
+var services = Services.default()
+
+try App.configure(&config, &env, &services)
+
+let app = try Application(
+    config: config,
+    environment: env,
+    services: services
+)
+
+try App.boot(app)
+
+try app.run()
